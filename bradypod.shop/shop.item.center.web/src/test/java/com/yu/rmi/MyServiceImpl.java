6bf13081d05b7cb24476c40ec7b9c2bb9d2a6322
previous_yu <previous_yu@163.com>
2015-10-17 00:57:20 +0800

    Signed-off-by: previous_yu <previous_yu@163.com>

--- bradypod.shop/shop.item.center.web/src/test/java/com/yu/rmi/MyServiceImpl.java
+++ bradypod.shop/shop.item.center.web/src/test/java/com/yu/rmi/MyServiceImpl.java
@@ -3,6 +3,7 @@ package com.yu.rmi;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class MyServiceImpl extends UnicastRemoteObject implements MyService {
@@ -17,12 +18,18 @@ public class MyServiceImpl extends UnicastRemoteObject implements MyService {
 		return "hello rmi";
 	}
 
-	public static void main(String[] args) throws RemoteException, MalformedURLException {
+	public static void main(String[] args) throws RemoteException,
+			MalformedURLException {
 		// 发布服务
-		MyServiceImpl serviceImpl = new MyServiceImpl();
-		// LocateRegistry.createRegistry(2001);
-		Naming.rebind("MyService", serviceImpl);
-		System.err.println("Read to start");
+		try {
+			MyServiceImpl serviceImpl = new MyServiceImpl();
+			LocateRegistry.createRegistry(2001);
+			Naming.rebind("rmi://192.168.64.1:2001/MyService", serviceImpl);
+			System.err.println("Read to start");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 
 }
--- bradypod.shop/shop.item.center.web/src/test/java/com/yu/rmi/RMITest.java
+++ bradypod.shop/shop.item.center.web/src/test/java/com/yu/rmi/RMITest.java
@@ -8,7 +8,7 @@ import java.rmi.registry.Registry;
 public class RMITest {
 
 	public static void main(String[] args) throws IOException, NotBoundException {
-		Registry registry = LocateRegistry.getRegistry(7000);
+		Registry registry = LocateRegistry.getRegistry(2001);
 		MyService stub = (MyService) registry.lookup("MyService");
 		String response = stub.say();
 		System.out.println("response: " + response);
