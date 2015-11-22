db50dd4803267ea3533ee011c97f31cf64830ad7
previous_yu <previous_yu@163.com>
2015-11-23 07:24:02 +0800

    Signed-off-by: previous_yu <previous_yu@163.com>

---
+++ bradypod.shop/shop.search/src/main/java/test/IndexManager.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class IndexManager {
+	
+    
+}
