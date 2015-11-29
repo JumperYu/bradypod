cdf78adbab287554feab46ebd05e6de422d99ee6
previous_yu <previous_yu@163.com>
2015-11-29 22:33:21 +0800

    Signed-off-by: previous_yu <previous_yu@163.com>

--- bradypod.shop/shop.search/src/main/java/com/bradypod/search/lucene/Searching.java
+++ bradypod.shop/shop.search/src/main/java/com/bradypod/search/lucene/Searching.java
@@ -8,6 +8,8 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
@@ -18,19 +20,24 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class Searching {
-	
+
 	public static void main(String[] args) {
-		Searching.search("E://index", "test");
+		Searching.deleteTest();
+//		Searching.search("E://index", "1.txt");
 	}
-	
-	public static void search(String indexDir, String q) {
+
+	/**
+	 * @param indexDir
+	 * @param q
+	 */
+	public static void search(String indexDir, String text) {
 		try {
 			Analyzer analyzer = new StandardAnalyzer();
 			Directory directory = FSDirectory.open(Paths.get(indexDir));
 			IndexReader reader = DirectoryReader.open(directory);
 			IndexSearcher searcher = new IndexSearcher(reader);
 			QueryParser parser = new QueryParser("filename", analyzer);
-			Query query = parser.parse(q);
+			Query query = parser.parse(text);
 			TopDocs topDocs = searcher.search(query, 1000);
 			System.out.println("总共匹配多少个：" + topDocs.totalHits);
 			ScoreDoc[] hits = topDocs.scoreDocs;
@@ -50,5 +57,42 @@ public class Searching {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public static void deleteTest(){
+		IndexWriter writer = getWriter("E://index");
+		try {
+			System.out.println("writer has docs : " + writer.numDocs());
+//			writer.deleteDocuments(new Term("id", "1"));
+			writer.deleteAll();
+			writer.commit();
+			System.out.println("writer has dele:" + writer.hasDeletions());
+			System.out.println("writer has docs : " + writer.maxDoc());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally{
+			try {
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 获取写操作
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public static IndexWriter getWriter(String index) {
+		IndexWriter writer = null;
+		try {
+			Directory directory = FSDirectory.open(Paths.get(index));
+			writer = new IndexWriter(directory, new IndexWriterConfig(
+					new StandardAnalyzer()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return writer;
+	}
 }
