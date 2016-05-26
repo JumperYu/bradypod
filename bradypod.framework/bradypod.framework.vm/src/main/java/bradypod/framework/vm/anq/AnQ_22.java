01eebd27f6a1a35b7e1a8f6b344c3b7338660e4f
previous_yu <previous_yu@163.com>
2016-05-26 14:12:11 +0800

    Signed-off-by: previous_yu <previous_yu@163.com>

---
+++ bradypod.framework/bradypod.framework.vm/src/main/java/bradypod/framework/vm/anq/AnQ_22.java
@@ -0,0 +1,23 @@
+package bradypod.framework.vm.anq;
+
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+/**
+ * HashMapµƒŒ Ã‚
+ * 
+ * @author xiangmin.zxm
+ *
+ */
+public class AnQ_22 {
+
+	public static void main(String[] args) {
+
+		Map<String, String> m = new IdentityHashMap<String, String>();
+		m.put("Mickey", "Mouse");
+		m.put("Mickey", "Mantle");
+		System.out.println(m.size());
+
+	}
+
+}
