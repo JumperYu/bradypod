d953e7ea60831fae49086888160d12f6f4700e85
previous_yu <previous_yu@163.com>
2016-07-06 21:04:11 +0800

    Signed-off-by: previous_yu <previous_yu@163.com>

--- bradypod.framework/framework.config/src/main/java/com/bradypod/framework/config/job/CheckingFileJob.java
+++ bradypod.framework/framework.config/src/main/java/com/bradypod/framework/config/job/CheckingFileJob.java
@@ -10,19 +10,21 @@ public class CheckingFileJob {
 
 	static ThreadLocal<String> holder = new ThreadLocal<>();
 
-	static boolean compileAndSet(String path) {
+	public static boolean beChanged(String path) {
 		try {
 			byte[] bytes = Files.readAllBytes(Paths.get(path));
 			String md5Hex = DigestUtils.md5Hex(bytes);
-			if (holder.get() != null && holder.get().equals(md5Hex)) {
-				return true;
-			} else{
-				holder.set(value);
+			if (holder.get() == null) {
+				holder.set(md5Hex);
+				return false;
+			}
+			if (holder.get().equals(md5Hex)) {
+				return false;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return false;
+		return true;
 	}
 
 }
--- bradypod.framework/framework.config/src/test/java/framework/config/schedule/TestSchedule.java
+++ bradypod.framework/framework.config/src/test/java/framework/config/schedule/TestSchedule.java
@@ -5,31 +5,36 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import com.bradypod.framework.config.job.CheckingFileJob;
+
 public class TestSchedule {
 
 	public static void main(String[] args) throws InterruptedException {
+		final String path = "D://a.txt";
 		ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 		scheduledExecutorService.schedule(new Callable<Void>() {
 			@Override
 			public Void call() throws Exception {
-				setFlag(true);
+				setTimeOut(true);
 				return null;
 			}
-		}, 5, TimeUnit.SECONDS);
+		}, 30, TimeUnit.SECONDS);
 		scheduledExecutorService.shutdown();
-		while (!flag) {
-			System.out.println("1");
-			TimeUnit.MILLISECONDS.sleep(100);
+		while (!isTimeOut && !isChanged) {
+			isChanged = CheckingFileJob.beChanged(path);
 		}
+		System.out.println("isChanged: " + isChanged + ", isTimeOut:" + isTimeOut);
 	}
 
-	static boolean flag = false;
+	static boolean isTimeOut = false;
+	static boolean isChanged = false;
 
-	public static void setFlag(boolean flag) {
-		TestSchedule.flag = flag;
+	public static boolean isTimeOut() {
+		return isTimeOut;
 	}
 
-	public static boolean getFlag() {
-		return flag;
+	public static void setTimeOut(boolean isTimeOut) {
+		TestSchedule.isTimeOut = isTimeOut;
 	}
+
 }
