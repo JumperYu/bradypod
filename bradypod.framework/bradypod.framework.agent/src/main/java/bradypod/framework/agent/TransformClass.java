52a18cfd59cc7c6da62e7fe1a5ea1a000fc286cf
previous_yu <previous_yu@163.com>
2016-09-11 22:30:01 +0800

    添加代理的实现

---
+++ bradypod.framework/bradypod.framework.agent/src/main/java/bradypod/framework/agent/TransformClass.java
@@ -0,0 +1,16 @@
+package bradypod.framework.agent;
+
+/**
+ * @see Transformer.java
+ * 
+ * @author	zengxm<http://github.com/JumperYu>
+ *
+ * @date	2016年9月11日
+ */
+public class TransformClass {
+
+	public int getNumber() {
+		return 1;
+	}
+
+}
