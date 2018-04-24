d1ac36283bdb95d9f7454a798e52d6c3b5bb9674
曾湘敏 <zengxiangmin@cvte.com>
2018-04-24 14:40:26 +0800

    增加配置中心

---
+++ bradypod.springcloud/cloud-client/src/main/java/springcloud/ConfigClientApplication.java
@@ -0,0 +1,25 @@
+package springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    // git配置文件里的key
+    @Value("${service-consumer}")
+    String name;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return name;
+    }
+}
